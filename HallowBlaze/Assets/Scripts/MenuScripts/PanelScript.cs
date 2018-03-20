f49962deaa8eed6426e5ed37da2a1de5dc0847c7
Furjoza <riderofthedark@gmail.com>
2018-03-20 22:18:17 +0100

    Fixing usage of keyboard in sub-menus. Adding some color to selected items in menu.

--- HallowBlaze/Assets/Scripts/MenuScripts/PanelScript.cs
+++ HallowBlaze/Assets/Scripts/MenuScripts/PanelScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class MenuScript : MonoBehaviour {
+public class PanelScript : MonoBehaviour {
 
     public EventSystem eventSystem;
     public GameObject selectedObject;
